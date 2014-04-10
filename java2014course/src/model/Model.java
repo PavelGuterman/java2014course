a90c7a5401d2bffd2f9be2f89e771b7a3830a426
zo.dennis <zo.dennis@62fc50ac-f662-48ca-2475-87b3ad4f9104>
2014-04-10 21:17:03 +0000

    מחלקה Model שב package הראשי model . כאן אנו לא מצפים לסוגים שונים של מודלים

--- java2014course/src/model/Model.java
+++ java2014course/src/model/Model.java
@@ -1,5 +1,26 @@
 package model;
 
-public class Model {
+import model.algorithms.Distance;
+import model.algorithms.Domain;
+import model.algorithms.Searcher;
 
+public class Model {
+	
+	private Domain domain;
+	private Distance[] distances;
+	private Searcher searcher;
+	
+	public void setDomain(String key){
+		
+	}
+	public void setHeuristics(String[] keys){
+		
+	}
+	public void setSearcher(String key){
+		
+	}
+	public int solveDomain(){
+		
+		return 0;
+	}
 }
