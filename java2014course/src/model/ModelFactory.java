f3e3568813f8f2241f6bf182013a39da261b3a5f
zo.dennis <zo.dennis@62fc50ac-f662-48ca-2475-87b3ad4f9104>
2014-04-12 18:16:20 +0000

    modelFactory

--- java2014course/src/model/ModelFactory.java
+++ java2014course/src/model/ModelFactory.java
@@ -1,5 +1,14 @@
 package model;
 
 public class ModelFactory {
+	private interface Creator {
+		public Model create();
+	}
+
+	private class DomainCreator implements Creator {
+		public Model create() {
+			return new Domain;
+		}
+	}
 
 }
