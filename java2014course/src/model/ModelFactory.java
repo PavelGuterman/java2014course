a3cd7df62fc299f243f8346160d7ea2670eb78b7
zo.dennis <zo.dennis@62fc50ac-f662-48ca-2475-87b3ad4f9104>
2014-04-13 19:11:27 +0000

    ModelFactory - set factory pattern

--- java2014course/src/model/ModelFactory.java
+++ java2014course/src/model/ModelFactory.java
@@ -1,39 +1,93 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import maze.Maze;
+import maze.MazeBig;
 import maze.MazeDistance;
 import maze.MazeDomain;
+import model.algorithms.Action;
 import model.algorithms.Distance;
 import model.algorithms.Domain;
+import model.algorithms.Searcher;
+import model.algorithms.State;
+import model.algorithms.a_star.Astar;
 
 public class ModelFactory {
-	HashMap<String,Creator> domainCreators;
+	HashMap<String,Domain_Creator> domainCreators;
+	HashMap<String,Searcher_Creator> sercherCreators;
+	HashMap<String,Distance_Creator> distanceCreators;
 	
-	
-	private interface Creator {
+	//Domain 
+	private interface Domain_Creator {
 		public Domain create();
 	}
-
 	
-	private class SmallMaze_Creator implements Creator {
+	private class SmallMaze_Creator implements Domain_Creator {
 		public Domain create() {
-			
 			return new MazeDomain(new Maze());
 		}
 	}
-	
-	
-	
-	
-	public ModelFactory(){
-		domainCreators = new HashMap<String, Creator>();
-		
-		domainCreators.put("small", new SmallMaze_Creator());
-
-		
-
+	private class BiglMaze_Creator implements Domain_Creator {
+		public Domain create() {
+			return new MazeDomain(new MazeBig());
+		}
 	}
 	
+	//sercher	
+	private interface Searcher_Creator {
+		public Searcher create(Domain domain, Distance g, Distance h);
+	}
+	
+	private  class AStar_Creator implements Searcher_Creator{
+		public Searcher create(Domain domain, Distance g, Distance h) {
+			return new Astar(domain, g, h);
+		}
+	}
+	
+	private  class BFS_Creator implements Searcher_Creator{
+		public Searcher create(Domain domain,Distance g, Distance h){
+			return new Astar(domain, g, h);
+		}
+	}
+	
+	public ModelFactory(){
+		domainCreators = new HashMap<String, Domain_Creator>();
+		domainCreators.put("small", new SmallMaze_Creator());
+		domainCreators.put("big", new BiglMaze_Creator());
+		
+		
+	}
+	
+	public void GetSerchers(Domain domain,Distance g, Distance h){
+		sercherCreators = new HashMap<String, Searcher_Creator>() ;
+		sercherCreators.put("astar", new AStar_Creator());
+		sercherCreators.put("bfs", new BFS_Creator());
+	}
+		
+	
+	//distance
+	private interface Distance_Creator {
+		public Distance create();
+	}
+	
+	
+	private  class MazeDistance_Creator implements Distance_Creator{
+		public Distance create() {
+			return new MazeDistance();
+		}
+	}
+	
+	private  class BFSDistance implements Distance_Creator{
+		public Distance create() {
+			return new MazeDistance();
+		}
+	}
+	
+	public void GetDistances(){
+		distanceCreators = new HashMap<String, Distance_Creator>() ;
+		distanceCreators.put("astar", new MazeDistance_Creator());
+		distanceCreators.put("bfs", new BFSDistance());
+	}
 }
